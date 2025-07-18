c97676e85138c4e66f5097d0106b76a68c918d57
Luiz Carvalho <beto_carvvalho@hotmail.com>
2025-07-18 00:00:06 -0300

    doc(swagger): add swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Menu Back')
+    .setDescription('Menu for your dinner')
+    .setVersion('1.0')
+    .addTag('Menu')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/shared/entities/user/user.entity.ts
+++ src/shared/entities/user/user.entity.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class UserEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id?: number;
 
+  @ApiProperty()
   @Column({ length: 80 })
   name: string;
 
+  @ApiProperty()
   @Column({ length: 80 })
   email: string;
 
+  @ApiProperty()
   @Column({ length: 100 })
   password: string;
 
+  @ApiProperty()
   @Column({ length: 100 })
   telephone: string;
 }
